14aba1a5c159c3bef24427b055b7a31075cd9205
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2022-06-02 16:08:23 +0300

    1

---
+++ laundryApp/laundryApp/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using laundryApp.View.windows;
+using laundryApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace laundryApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+
+                //wd_setupFirstPos logIn = new wd_setupFirstPos();
+
+                wd_logIn logIn = new wd_logIn();
+                //MainWindow logIn = new MainWindow();
+                logIn.Show();
+
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+    }
+}
