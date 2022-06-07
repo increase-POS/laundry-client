86faac3f2b2c4cec2c623572789b28d7cedb4ebb
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2022-06-07 17:48:04 +0300

    Create services.cs

---
+++ laundryApp/laundryApp/Classes/ApiClasses/services.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laundryApp.Classes.ApiClasses
+{
+    public class services
+    {
+        public int serviceId { get; set; }
+        public string name { get; set; }
+        public string notes { get; set; }
+        public byte isActive { get; set; }
+        public decimal price { get; set; }
+        public decimal cost { get; set; }
+        public Nullable<int> categoryId { get; set; }
+        public System.DateTime createDate { get; set; }
+        public Nullable<System.DateTime> updateDate { get; set; }
+        public Nullable<int> createUserId { get; set; }
+        public Nullable<int> updateUserId { get; set; }
+    }
+}
